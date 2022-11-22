7235d06269cf2e54a27064ddb73a3197b3e6a944
icaro-freitas <icaro_freitas2014@outlook.com>
2022-11-21 22:00:30 -0300

    Alteracao da descricao das rainhas

--- Rainhas/src/rainhas/Tabuleiro_Rainhas.java
+++ Rainhas/src/rainhas/Tabuleiro_Rainhas.java
@@ -98,7 +98,7 @@ public class Tabuleiro_Rainhas extends javax.swing.JFrame {
     public void escreverNaInterface() {
         if (testarTabuleiro()) {
             jLabel1.setForeground(Color.red);
-            jLabel1.setText("Uma rainha esta em cheque!");
+            jLabel1.setText("Alguma rainha esta em cheque!");
         } else {
             jLabel1.setForeground(Color.black);
             jLabel1.setText("Nenhuma rainha esta em cheque!");
